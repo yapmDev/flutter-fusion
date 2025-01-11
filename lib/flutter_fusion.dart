2b09db52f094bb4cfd20d6e5f781b8de72a76f46
yapmDev <yosmelperez128@gmail.com>
2025-01-11 17:08:03 -0500

    added OnboardingScreen widget

--- lib/flutter_fusion.dart
+++ lib/flutter_fusion.dart
@@ -3,3 +3,4 @@ library flutter_fusion;
 export 'src/toolbox/random.dart';
 export 'src/custom_widgets/toast.dart';
 export 'src/custom_widgets/safe_status_bar.dart';
+export 'src/custom_widgets/onboarding_screen.dart';
