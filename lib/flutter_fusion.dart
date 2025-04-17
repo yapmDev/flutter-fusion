e78734c72c96c4950d642179b9d6202722e10972
yapmDev <yosmelperez128@gmail.com>
2025-04-17 00:27:13 -0400

    moved Location Picker to an individual repository

--- lib/flutter_fusion.dart
+++ lib/flutter_fusion.dart
@@ -5,4 +5,3 @@ export 'src/custom_widgets/toast.dart';
 export 'src/custom_widgets/status_bar_controller.dart';
 export 'src/custom_widgets/onboarding_screen.dart';
 export 'src/custom_widgets/keyboard_layout_builder.dart';
-export 'src/custom_widgets/location_picker.dart';
